6500e719647c9ff5550778415c4412a6e5f1af58
Maksim <maks_gr@rambler.ru>
2019-08-08 14:23:14 +0400

    Initial Commit

---
+++ Home work 2.6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Home work 2.6
+//
+//  Created by Maksim Grebenozhko on 08/08/2019.
+//  Copyright © 2019 Maksim Grebenozhko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
