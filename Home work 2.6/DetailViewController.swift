c5b53b823c5748d5015b01081cf59a47f9aa4800
Maksim <maks_gr@rambler.ru>
2019-08-09 21:25:59 +0400

    Task 1 ready

---
+++ Home work 2.6/DetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  DetailViewController.swift
+//  Home work 2.6
+//
+//  Created by Maksim Grebenozhko on 09/08/2019.
+//  Copyright © 2019 Maksim Grebenozhko. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var surnameLabel: UILabel!
+    @IBOutlet var emailLabel: UILabel!
+    @IBOutlet var phoneNumberLabel: UILabel!
+    @IBOutlet var photoView: UIImageView!
+
+    var person: Person!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        fillWithData()
+    }
+    
+    private func fillWithData() {
+        if person != nil {
+            nameLabel.text = person.name
+            surnameLabel.text = person.surname
+            emailLabel.text = person.email
+            phoneNumberLabel.text = person.phoneNumber
+            photoView.image = UIImage(named: person.photo)
+        }
+    }
+}
--- Home work 2.6/MainViewController.swift
+++ Home work 2.6/MainViewController.swift
@@ -21,17 +21,21 @@ struct Person {
     static var phonNumbers = ["+74957296019", "+79021005421", "+79031936251", "+79046377386", "+79052975318",
                               "+79062981070", "+79062983093", "+79062983656", "+79062994716", "+79062996257"]
     
+    static var photos = ["mett-dejmon", "bred-pitt", "dzhek-nikolson", "dzhonni-depp", "kevin-kostner",
+     "leonardo-di-kaprio", "nikolas-kejdzh", "robert-de-niro", "tom-henks", "tom-kruz"]
+    
     var name = ""
     var surname = ""
     var email = ""
     var phoneNumber = ""
+    var photo = ""
     
     init() {
         name = takeItemFrom(array: &Person.names)
         surname = takeItemFrom(array: &Person.surnames)
         email = makeEmail()
         phoneNumber = takeItemFrom(array: &Person.phonNumbers)
-        print("\(name) \(surname) \(email) \(phoneNumber)")
+        photo = takePhoto()
     }
     
     private func takeItemFrom(array: inout [String]) -> String {
@@ -41,13 +45,27 @@ struct Person {
             array.remove(at: index)
             return personName
         } else {
-            return "null"
+            return ""
+        }
+    }
+    private func takePhoto() -> String {
+        if name != "" {
+            let index = Int.random(in: 0..<Person.photos.count)
+            let photo = Person.photos[index]
+            Person.photos.remove(at: index)
+            return photo
+        } else {
+            return ""
         }
     }
     private func makeEmail() -> String {
-        let index = Int.random(in: 0..<Person.servers.count)
-        let server = Person.servers[index]
-        return ("\(name)\(surname)@\(server)")
+        if name != "" {
+            let index = Int.random(in: 0..<Person.servers.count)
+            let server = Person.servers[index]
+            return ("\(name)\(surname)@\(server)")
+        } else {
+            return ""
+        }
     }
 }
 
@@ -59,6 +77,7 @@ class MainViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        navigationItem.leftBarButtonItem = editButtonItem
         createPersons()
     }
     
@@ -67,16 +86,38 @@ class MainViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         
         cell.textLabel?.text = "\(persons[indexPath.row].name) \(persons[indexPath.row].surname)"
+        let photo = persons[indexPath.row].photo
+        if photo != "" {
+            cell.imageView?.image = UIImage(named: photo)
+            cell.imageView?.contentMode = .scaleToFill // не работает, как размеры сделать одинковыми?
+        }
         
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let currentPerson = persons.remove(at: sourceIndexPath.row)
+        persons.insert(currentPerson, at: destinationIndexPath.row)
+        tableView.reloadData()
+    }
+    
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .none
     }
     
     private func createPersons() {
         for _ in 0..<countOfPersons {
             persons.append(Person())
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let indexPath = tableView.indexPathForSelectedRow {
+            let detailVC = segue.destination as! DetailViewController
+            detailVC.person = persons[indexPath.row]
         }
     }
 }
