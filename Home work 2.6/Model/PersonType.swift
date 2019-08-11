757461196ae95ab8822d70438b92c0790889df0e
Maksim <maks_gr@rambler.ru>
2019-08-11 22:39:36 +0400

    Ready for review2

---
+++ Home work 2.6/Model/PersonType.swift
@@ -0,0 +1,18 @@
+//
+//  PersonType.swift
+//  Home work 2.6
+//
+//  Created by Maksim Grebenozhko on 11/08/2019.
+//  Copyright © 2019 Maksim Grebenozhko. All rights reserved.
+//
+
+import Foundation
+
+struct Person {
+ 
+    let name: String
+    let surname: String
+    let email: String
+    let phoneNumber: String
+    let photo: String
+}
---
+++ Home work 2.6/View/PersonTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PersonTableViewCell.swift
+//  Home work 2.6
+//
+//  Created by Maksim Grebenozhko on 11/08/2019.
+//  Copyright © 2019 Maksim Grebenozhko. All rights reserved.
+//
+
+import UIKit
+
+class PersonTableViewCell: UITableViewCell {
+
+    @IBOutlet private var photoImageView: UIImageView!
+    @IBOutlet private var emailLabel: UILabel!
+    @IBOutlet private var phoneNumberLabel: UILabel!
+
+    func configure(person: Person) {
+        if person.photo != "" {
+            photoImageView.image = UIImage(named: person.photo)
+        }
+        emailLabel.text = "\(person.email)"
+        phoneNumberLabel.text = "\(person.phoneNumber)"
+    }
+}
